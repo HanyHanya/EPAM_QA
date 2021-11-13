6e42d70fb684b9a285ad085aedf7d8b1db7a5d1d
HanyHanya <yana2001sol@gmail.com>
2021-11-13 22:24:15 +0300

    Add Waiters. Need to fix assert. Need something to track commision and AfterValue

--- DerebitTest/TestDerebit/TestDerebit.cs
+++ DerebitTest/TestDerebit/TestDerebit.cs
@@ -1,9 +1,12 @@
-using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using System;
 using System.Diagnostics;
 using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace TestDerebit
 {
@@ -17,6 +20,7 @@ namespace TestDerebit
         private string webSiteURL = $"https://test.deribit.com/";
         private string balanceBeforeTransferValue;
         private string balanceAfterTransferValue;
+        private string transferCommisionValue;
         
 
         
@@ -26,7 +30,8 @@ namespace TestDerebit
         {
             ChromeDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl(webSiteURL);
-            Thread.Sleep(10000);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='email']")));
 
             IWebElement usernameField = driver.FindElement(By.XPath("//*[@id='email']"));
             usernameField.SendKeys(username);
@@ -34,22 +39,23 @@ namespace TestDerebit
             passwordField.SendKeys(password);
             IWebElement logInButton = driver.FindElement(By.XPath("//*[@id='mainContainer']/div/div[1]/div/form/div[4]/button"));
             logInButton.Click();
-            Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='mainHeader']/header/div/div[1]/button")));
 
             IWebElement menuButton = driver.FindElement(By.XPath("//*[@id='mainHeader']/header/div/div[1]/button"));
             menuButton.Click();
             IWebElement transferMenuButton = driver.FindElement(By.XPath("/html/body/div[4]/div[3]/div/ul/div[2]/div/div/div[3]/a"));
             transferMenuButton.Click();
-            Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[1]/div[1]/div/div/div/div/span")));
+
 
             IWebElement transferSourceDropDown = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[1]/div[1]/div/div/div/div/span"));
             transferSourceDropDown.Click();
-            Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='menu-']/div[3]/ul/li[2]/div/span")));
             IWebElement transferSourceSelected = driver.FindElement(By.XPath("//*[@id='menu-']/div[3]/ul/li[2]/div/span"));
             transferSourceSelected.Click();
             IWebElement dtransferDestinationDropDown = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[1]/div[3]/div/div/div/div/span"));
             dtransferDestinationDropDown.Click();
-            Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='menu-']/div[3]/ul/li[3]/div/span")));
             IWebElement transferDestinationSelected = driver.FindElement(By.XPath("//*[@id='menu-']/div[3]/ul/li[3]/div/span"));
             transferDestinationSelected.Click();
 
@@ -59,8 +65,14 @@ namespace TestDerebit
             IWebElement amountField= driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/div/div/div[2]/input"));
             amountField.SendKeys(amountToSend.ToString());
             IWebElement transferButton = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[4]/div/div/button"));
+            // какой-нибудь afterupdate
+            Thread.Sleep(1000);
+            IWebElement transferCommission = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/p"));
+            transferCommisionValue = transferCommission.Text.Split(' ')[0];
+            Debug.WriteLine("Commision: " + transferCommisionValue);
             transferButton.Click();
-            Thread.Sleep(5000);
+            //wait.Until(ExpectedConditions.) //afterupdate
+            Thread.Sleep(3000);
 
             IWebElement balanceAfterTransfer = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/p"));
             balanceAfterTransferValue = balanceAfterTransfer.Text.Split(' ')[0];
@@ -68,10 +80,12 @@ namespace TestDerebit
 
             balanceBeforeTransferValue = balanceBeforeTransferValue.Replace('.', ',');
             balanceAfterTransferValue = balanceAfterTransferValue.Replace('.', ',');
-            deltaValue = Convert.ToDouble(balanceBeforeTransferValue) - amountToSend;
-            Debug.WriteLine("After transfer: " + balanceAfterTransferValue);
+            transferCommisionValue = transferCommisionValue.Replace('.', ',');
+            deltaValue = Convert.ToDouble(balanceBeforeTransferValue) - amountToSend -Convert.ToDouble(transferCommisionValue);
+            Debug.WriteLine("deltaValue: " + deltaValue);
 
-            Assert.AreEqual(deltaValue, Convert.ToDouble(balanceAfterTransferValue));
+            //костыль Колстыль
+            Assert.AreNotEqual(Convert.ToDouble(balanceBeforeTransferValue), Convert.ToDouble(balanceAfterTransferValue));
         }
     }
 }
