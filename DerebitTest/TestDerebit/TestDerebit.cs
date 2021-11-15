ab8ee42bedd20d3fd9386767c27db884242f9f57
HanyHanya <yana2001sol@gmail.com>
2021-11-15 19:06:46 +0300

    fix some XPath

--- DerebitTest/TestDerebit/TestDerebit.cs
+++ DerebitTest/TestDerebit/TestDerebit.cs
@@ -23,7 +23,7 @@ namespace TestDerebit
         private string transferCommisionValue;
 
         ChromeDriver driver;
-
+        // поменять поиск
         [Test]
         public void TransferTest()
         {
@@ -36,52 +36,55 @@ namespace TestDerebit
             usernameField.SendKeys(username);
             IWebElement passwordField = driver.FindElement(By.XPath("//*[@id='password']"));
             passwordField.SendKeys(password);
-            IWebElement logInButton = driver.FindElement(By.XPath("//*[@id='mainContainer']/div/div[1]/div/form/div[4]/button"));
+            IWebElement logInButton = driver.FindElement(By.XPath("//button[@data-id='login']"));
             logInButton.Click();
-            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='mainHeader']/header/div/div[1]/button")));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//button[@data-id='sideMenuBtn']")));
 
-            IWebElement menuButton = driver.FindElement(By.XPath("//*[@id='mainHeader']/header/div/div[1]/button"));
+            IWebElement menuButton = driver.FindElement(By.XPath("//button[@data-id='sideMenuBtn']"));
             menuButton.Click();
-            IWebElement transferMenuButton = driver.FindElement(By.XPath("/html/body/div[4]/div[3]/div/ul/div[2]/div/div/div[3]/a"));
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@data-id='transfer']")));
+            IWebElement transferMenuButton = driver.FindElement(By.XPath("//a[@data-id='transfer']"));
             transferMenuButton.Click();
-            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[1]/div[1]/div/div/div/div/span")));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@class='MuiListItemText-root'  and contains(span, 'Please select a source')]")));
 
 
-            IWebElement transferSourceDropDown = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[1]/div[1]/div/div/div/div/span"));
+            IWebElement transferSourceDropDown = driver.FindElement(By.XPath("//div[@class='MuiListItemText-root'  and contains(span, 'Please select a source')]"));
             transferSourceDropDown.Click();
             wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='menu-']/div[3]/ul/li[2]/div/span")));
-            IWebElement transferSourceSelected = driver.FindElement(By.XPath("//*[@id='menu-']/div[3]/ul/li[2]/div/span"));
+            IWebElement transferSourceSelected = driver.FindElements(By.XPath("//div[@class='MuiListItemText-root'  and contains(span, 'Kiroki')]"))[0];
             transferSourceSelected.Click();
-            IWebElement dtransferDestinationDropDown = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[1]/div[3]/div/div/div/div/span"));
+            IWebElement dtransferDestinationDropDown = driver.FindElement(By.XPath("//div[@class='MuiListItemText-root'  and contains(span, 'Please select a destination')]"));
             dtransferDestinationDropDown.Click();
             wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='menu-']/div[3]/ul/li[3]/div/span")));
-            IWebElement transferDestinationSelected = driver.FindElement(By.XPath("//*[@id='menu-']/div[3]/ul/li[3]/div/span"));
+            IWebElement transferDestinationSelected = driver.FindElement(By.XPath("//div[@class='MuiListItemText-root'  and contains(span, 'Kiroki_1')]"));
             transferDestinationSelected.Click();
 
-            IWebElement balanceBeforeTransfer = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[3]/div[2]/p"));
+            IWebElement balanceBeforeTransfer = driver.FindElement(By.XPath("//p[@data-id='tradingBalance']"));
             balanceBeforeTransferValue = balanceBeforeTransfer.Text.Split(' ')[0];
             Debug.WriteLine("Before transfer: " + balanceBeforeTransferValue);
-            IWebElement amountField= driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/div/div/div[2]/input"));
+            IWebElement amountField= driver.FindElement(By.XPath("//input[@class='MuiInputBase-input']"));
             amountField.SendKeys(amountToSend.ToString());
-            IWebElement transferButton = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[4]/div/div/button"));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[@data-id='transferBtn']")));
+            IWebElement transferButton = driver.FindElement(By.XPath("//button[@data-id='transferBtn']"));
             // какой-нибудь afterupdate
-            Thread.Sleep(1000);
-            IWebElement transferCommission = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/p"));
-            transferCommisionValue = transferCommission.Text.Split(' ')[0];
-            Debug.WriteLine("Commision: " + transferCommisionValue);
+            //Thread.Sleep(1000);
+            //IWebElement transferCommission = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/p"));
+            //transferCommisionValue = transferCommission.Text.Split(' ')[0];
+            //Debug.WriteLine("Commision: " + transferCommisionValue);
             transferButton.Click();
             //wait.Until(ExpectedConditions.) //afterupdate
             Thread.Sleep(3000);
 
-            IWebElement balanceAfterTransfer = driver.FindElement(By.XPath("//*[@id='mainBody']/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/p"));
+            IWebElement balanceAfterTransfer = driver.FindElement(By.XPath("//p[@data-id='availableBalance']"));
             balanceAfterTransferValue = balanceAfterTransfer.Text.Split(' ')[0];
             Debug.WriteLine("After transfer: " + balanceAfterTransferValue);
 
             balanceBeforeTransferValue = balanceBeforeTransferValue.Replace('.', ',');
             balanceAfterTransferValue = balanceAfterTransferValue.Replace('.', ',');
-            transferCommisionValue = transferCommisionValue.Replace('.', ',');
-            deltaValue = Convert.ToDouble(balanceBeforeTransferValue) - amountToSend -Convert.ToDouble(transferCommisionValue);
-            Debug.WriteLine("deltaValue: " + deltaValue);
+            //transferCommisionValue = transferCommisionValue.Replace('.', ',');
+            //deltaValue = Convert.ToDouble(balanceBeforeTransferValue) - amountToSend -Convert.ToDouble(transferCommisionValue);
+            //Debug.WriteLine("deltaValue: " + deltaValue);
 
             //Assert.AreEqual(deltaValue, Convert.ToDouble(balanceAfterTransferValue));
             Assert.AreNotEqual(Convert.ToDouble(balanceBeforeTransferValue), Convert.ToDouble(balanceAfterTransferValue));
